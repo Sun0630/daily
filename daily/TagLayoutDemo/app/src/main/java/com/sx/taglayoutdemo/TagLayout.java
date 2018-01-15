9753790387e15f9c2025d9734cafec229195a640
sunxin <sunxin0630@126.com>
2018-01-15 15:54:11 +0800

    taglayout

--- daily/TagLayoutDemo/app/src/main/java/com/sx/taglayoutdemo/TagLayout.java
+++ daily/TagLayoutDemo/app/src/main/java/com/sx/taglayoutdemo/TagLayout.java
@@ -2,8 +2,12 @@ package com.sx.taglayoutdemo;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Administrator
@@ -12,6 +16,9 @@ import android.view.ViewGroup;
  */
 
 public class TagLayout extends ViewGroup {
+
+
+    private List<List<View>> mChildViews = new ArrayList<>();
 
     public TagLayout(Context context) {
         super(context);
@@ -25,40 +32,71 @@ public class TagLayout extends ViewGroup {
         super(context, attrs, defStyleAttr);
     }
 
+    // 2.1 onMeasure() 指定宽高
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        // 清空集合
+        mChildViews.clear();
+
         int childCount = getChildCount();
 
-        //获取到宽高
+        // 获取到宽度
         int width = MeasureSpec.getSize(widthMeasureSpec);
 
-        //计算高度
-        int height = getPaddingBottom() + getPaddingTop();
+        // 高度需要计算
+        int height = getPaddingTop() + getPaddingBottom();
 
-        //一行的宽度
+        // 一行的宽度
         int lineWidth = getPaddingLeft();
 
-        //for循环测量子View
+        ArrayList<View> childViews = new ArrayList<>();
+        mChildViews.add(childViews);
+
+        // 子View高度不一致的情况下
+        int maxHeight = 0;
+
         for (int i = 0; i < childCount; i++) {
+            // 2.1.1 for循环测量子View
             View childView = getChildAt(i);
-            //这句代码执行之后就可以获取子View的宽高
+            // 这段话执行之后就可以获取子View的宽高，因为会调用子View的onMeasure
             measureChild(childView, widthMeasureSpec, heightMeasureSpec);
 
-            //根据子View执行和指定自己的布局  margine
-            if (lineWidth + childView.getMeasuredWidth() > width) {
-                //换行,高度需要累加
-                height += childView.getMeasuredHeight();
-                //行宽置为0
-                lineWidth = 0;
+            // margin值 ViewGroup.LayoutParams 没有 就用系统的MarginLayoutParams
+            // 想想 LinearLayout为什么有？
+            // LinearLayout有自己的 LayoutParams  会复写一个非常重要的方法
+            ViewGroup.MarginLayoutParams params = (MarginLayoutParams) childView.getLayoutParams();
+
+            // 什么时候需要换行，一行不够的情况下 考虑 margin
+            if (lineWidth + (childView.getMeasuredWidth() + params.rightMargin + params.leftMargin) > width) {
+                // 换行,累加高度  加上一行条目中最大的高度
+                height += childView.getMeasuredHeight() + params.bottomMargin + params.topMargin;
+                lineWidth = childView.getMeasuredWidth() + params.rightMargin + params.leftMargin;
+                childViews = new ArrayList<>();
+                mChildViews.add(childViews);
             } else {
-                lineWidth += childView.getMeasuredWidth();
+                lineWidth += childView.getMeasuredWidth() + params.rightMargin + params.leftMargin;
+                maxHeight = Math.max(childView.getMeasuredHeight() + params.bottomMargin + params.topMargin, maxHeight);
             }
+
+            childViews.add(childView);
         }
 
-        //指定自己的宽高
-        setMeasuredDimension(width,height);
+        height += maxHeight;
 
+        Log.e("TAG", "width -> " + width + " height-> " + height);
+        // 2.1.2 根据子View计算和指定自己的宽高
+        setMeasuredDimension(width, height);
+    }
+
+    @Override
+    protected LayoutParams generateLayoutParams(LayoutParams p) {
+        return super.generateLayoutParams(p);
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new MarginLayoutParams(getContext(), attrs);
     }
 
     /**
@@ -72,6 +110,49 @@ public class TagLayout extends ViewGroup {
      */
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        int left, top = getPaddingTop(), right, bottom;
 
+        for (List<View> childViews : mChildViews) {
+            left = getPaddingLeft();
+
+            for (View childView : childViews) {
+                ViewGroup.MarginLayoutParams params = (MarginLayoutParams) childView.getLayoutParams();
+                left += params.leftMargin;
+                int childTop = top + params.topMargin;
+                right = left + childView.getMeasuredWidth();
+                bottom = childTop + childView.getMeasuredHeight();
+                Log.e("TAG", childView.toString());
+
+                Log.e("TAG", "left -> " + left + " top-> " + childTop + " right -> " + right + " bottom-> " + bottom);
+
+                // 摆放
+                childView.layout(left, childTop, right, bottom);
+                // left 叠加
+                left += childView.getMeasuredWidth() + params.rightMargin;
+            }
+
+            // 不断的叠加top值
+            ViewGroup.MarginLayoutParams params = (MarginLayoutParams) childViews.get(0).getLayoutParams();
+            top += childViews.get(0).getMeasuredHeight() + params.topMargin + params.bottomMargin;
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
